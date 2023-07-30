a2561bad20de0990b99e41eb49252feccd05e000
Francisco Costa <francisco100eg@gmail.com>
2023-07-30 18:41:14 -0300

    :Initial commit- Starting project using React Native

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <ScrollView>
+      <Text>Currency App</Text>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
