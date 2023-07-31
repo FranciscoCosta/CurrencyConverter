b29b93b4fff61070f5b72add991e88dd1257db5b
Francisco Costa <francisco100eg@gmail.com>
2023-07-30 22:55:22 -0300

    :Feat: Finish app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
 import {currencyDataCard} from './Constants';
 import CurrencyCard from './components/CurrencyCard';
 import {fetchData} from './utils/fetchData';
@@ -7,7 +7,8 @@ import {fetchData} from './utils/fetchData';
 function App(): JSX.Element {
   const [currencyActive, setCurrencyActive] = useState('');
   const [isloading, setIsLoading] = useState(false);
-  const [currencyInfo, setCurrencyInfo] = useState([]);
+  const [currencyInfo, setCurrencyInfo] = useState<any>({});
+  const [currencyValue, setCurrencyValue] = useState(0);
 
   const getCurrencyInfo = async () => {
     const currencyInfoData = await fetchData(currencyActive);
@@ -23,6 +24,26 @@ function App(): JSX.Element {
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.appTitle}>Conversão BRL</Text>
+      <TextInput
+        style={styles.valueInput}
+        placeholder="Digite o valor"
+        keyboardType="numeric"
+        onChangeText={(text: string) => setCurrencyValue(+text)}
+      />
+      {currencyActive !== '' && (
+        <View style={styles.working}>
+          <View style={styles.display}>
+            <Text style={styles.displayText}>
+              1{currencyActive} = {currencyInfo.ask}BRL
+            </Text>
+          </View>
+          <Text style={styles.displayTextValue}>
+            {currencyValue} BRL ={' '}
+            {(Number(currencyValue) / Number(currencyInfo.ask)).toFixed(2)}{' '}
+            {currencyActive}
+          </Text>
+        </View>
+      )}
       <View style={styles.containerCards}>
         {!isloading &&
           currencyDataCard.map((currency: any) => (
@@ -40,13 +61,13 @@ function App(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#092a4a',
+    backgroundColor: '#46237A',
     padding: 10,
   },
   appTitle: {
     fontSize: 40,
     fontWeight: '800',
-    color: '#ff5500',
+    color: '#FF495C',
     alignSelf: 'center',
     marginTop: 10,
     letterSpacing: 2,
@@ -55,6 +76,37 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     flexDirection: 'row',
     width: '100%',
+    marginTop: 30,
+  },
+  working: {
+    backgroundColor: '#FCFCFC',
+    minHeight: 70,
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 30,
+  },
+  valueInput: {
+    backgroundColor: '#FCFCFC',
+    height: 50,
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 30,
+    color: '#000',
+  },
+  display: {
+    marginBottom: 10,
+    width: '100%',
+  },
+  displayText: {
+    color: '#2fc18c',
+    fontSize: 17,
+    fontWeight: '600',
+    textAlign: 'right',
+  },
+  displayTextValue: {
+    color: '#46237A',
+    fontSize: 25,
+    fontWeight: 'bold',
   },
 });
 
--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -4,6 +4,7 @@ export const fetchData = async (currency: string) => {
   const response = await axios.get(
     ` https://economia.awesomeapi.com.br/last/${currency}-BRL`,
   );
-  const data = response.data;
+  const result = response.data;
+  const data = Object.values(result)[0];
   return data;
 };
